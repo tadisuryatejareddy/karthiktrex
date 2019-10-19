f5f23760cc757d34bb7c91eace42a0126b357c9d
karthiks061992 <karthiks061992@gmail.com>
2019-10-19 14:04:35 +0530

    first commit

--- duplicate.py
+++ duplicate.py
@@ -3,10 +3,10 @@ def fun(x):
     size=len(x)
     for i in range(size):
         k=i+1;
-        for j in range(size,k):
-            if (x[i]==x[j] and not in repeated):
+        for j in range(k,size):
+            if (x[i]==x[j] and x[i] not in repeated):
                 repeated.append(x[i])
-    return repeated
+    return repeated;
                 
 print("the duplicate elements in the list are")
 list=[20,20,10,10]
